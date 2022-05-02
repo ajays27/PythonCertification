1a59ea9c314d6804b3660f7b9b0cf7b5f4a6c24f
Chaitali31 <chaitali.satpute@gmail.com>
2022-05-02 20:30:19 +0530

    Added comments

---
+++ Lists(arrays).py
@@ -0,0 +1,9 @@
+my_list = ["white", "purple", "blue", "yellow", "green"]
+
+for color in my_list:
+    print(color)
+
+my_sub_list = [1,"Array",[1,2,3,4,5],"MJ",5.4]
+
+for item in my_sub_list:
+    print(item)
