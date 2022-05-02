cac9d2e975b4a869de3a05130987537b15fa33b3
Chaitali31 <chaitali.satpute@gmail.com>
2022-05-02 20:21:57 +0530

    Bubble Sort and Reverse Bubble sort using Reverse() function

---
+++ BubbleSort.py
@@ -0,0 +1,18 @@
+num_list = []
+n = 0
+
+while n != -1:
+    n =int(input("Enter a number OR Enter -1 to stop -  "))
+    if n > 0:
+        num_list.append(n)
+
+print("\tOriginal List - \t",num_list)
+
+for i in range (0,len(num_list)):
+    for j in range(i,len(num_list)):
+        if num_list[i] > num_list[j]:
+            num_list[i],num_list[j] = num_list[j],num_list[i]
+
+print("\tSorted List - \t" ,num_list)
+num_list.reverse()
+print("\tReversed Sorted List - \t\t",num_list)
